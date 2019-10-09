0882c6fa3127ecc71299f530e3453f790dd6fb92
XiaoStevens <52208750+XiaoStevens@users.noreply.github.com>
2019-10-09 13:22:52 -0400

    Updates

---
+++ precache-manifest.f9b078a1489c5442ea38481ed4f0f6d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b37b020ab74140c564cfa6f81791b0",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f434ca88db948216a428",
+    "url": "/monster-rolodex/static/css/main.18ade7f0.chunk.css"
+  },
+  {
+    "revision": "736c910ed9b4345ab0d5",
+    "url": "/monster-rolodex/static/js/2.924cadc2.chunk.js"
+  },
+  {
+    "revision": "f434ca88db948216a428",
+    "url": "/monster-rolodex/static/js/main.d34cf995.chunk.js"
+  },
+  {
+    "revision": "6f70f97b8cfdb2d06998",
+    "url": "/monster-rolodex/static/js/runtime-main.aec4d7d7.js"
+  }
+]);
